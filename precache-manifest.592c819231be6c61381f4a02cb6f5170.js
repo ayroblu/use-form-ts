0970d0afc43c51a2ce7868e199d52a33abd0710a
ayroblu <ayroblu@users.noreply.github.com>
2020-11-29 18:20:39 +0000

    deploy: 9fb2432d79212223c5b49afe285861e412997c6b

---
+++ precache-manifest.592c819231be6c61381f4a02cb6f5170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60cc9c3883254a6c3a202c9f417e1ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b697b81f0d291eb40f16",
+    "url": "/static/css/main.abd1fe45.chunk.css"
+  },
+  {
+    "revision": "36d5bcf8adec479ea240",
+    "url": "/static/js/2.33e59119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33e59119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b697b81f0d291eb40f16",
+    "url": "/static/js/main.249f8af1.chunk.js"
+  },
+  {
+    "revision": "3f29df34566acb254e29",
+    "url": "/static/js/runtime-main.48dd4776.js"
+  }
+]);

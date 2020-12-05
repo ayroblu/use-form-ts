07d3aa4994c0040e4293358ae31ae3ab59247a60
Ben Lu <ayroblu@gmail.com>
2020-12-05 18:32:57 +0000

    Write some tests!

--- src/test/KitchenSink.tsx
+++ src/test/KitchenSink.tsx
@@ -4,12 +4,17 @@ import { useForm } from "../useForm";
 
 export const KitchenSink = () => {
   const [state, setState] = React.useState({ field: "" });
+  const [log, setLog] = React.useState<string[]>([]);
   const form = useForm({
     values: state,
     onChange: (value) => setState({ ...state, ...value }),
   });
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (form.validate()) setLog(log.concat(`Valid! ${JSON.stringify(state)}`));
+  };
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       {form.createFormItem("field", {
         adaptor: inputAdaptor,
         meta: { label: "Field" },
@@ -17,21 +22,33 @@ export const KitchenSink = () => {
         validationMessages: {
           required: ({ meta: { label } }) => `Yo, ${label} is required`,
         },
-        custom: (val) => (val === "3" ? "3 is not allowed!" : ""),
+        custom: (val) => (val === "3" ? "3 is not allowed!" : null),
         customAsync: (val) =>
-          wait().then(() => (val === "4" ? "4 is not allow async!" : "")),
+          wait().then(() => (val === "4" ? "4 is not allowed async!" : null)),
         validation: {
           type: "whitespace",
         },
-      })(({ meta: { label }, errorText, ...props }) => (
+      })(({ meta: { label }, isLoading, errorText, ...props }) => (
         <label>
           <span>{label}</span>
           <input {...props} />
-          <span>{errorText}</span>
+          {isLoading ? (
+            <span data-testid={testIds.loading}>...loading</span>
+          ) : null}
+          <span data-testid={testIds.errorText}>{errorText}</span>
         </label>
       ))}
-    </div>
+      <input type="submit" value="Submit" />
+      {log.map((l, i) => (
+        <div key={i}>{l}</div>
+      ))}
+    </form>
   );
 };
 const inputAdaptor = (e: React.ChangeEvent<HTMLInputElement>) => e.target.value;
 const wait = () => new Promise((y) => setTimeout(y));
+
+export const testIds = {
+  errorText: "errorText",
+  loading: "loading",
+};
--- src/test/useForm.test.tsx
+++ src/test/useForm.test.tsx
@@ -1,13 +1,93 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
-import "@testing-library/jest-dom";
 
-import { KitchenSink } from "./KitchenSink";
+import { KitchenSink, testIds } from "./KitchenSink";
 
 describe("useForm", () => {
   it("should render without errors", () => {
     render(<KitchenSink />);
 
-    expect(screen.getByRole("heading")).toHaveTextContent("Welcome, John Doe");
+    expect(screen.getByTestId(testIds.errorText)).toHaveTextContent("");
+  });
+
+  it("no errors for entering in '1'", () => {
+    render(<KitchenSink />);
+    const domNode = screen.getByLabelText("Field") as HTMLInputElement;
+    userEvent.type(domNode, "1");
+    expect(domNode.value).toEqual("1");
+
+    expect(screen.getByTestId(testIds.errorText)).toHaveTextContent("");
+
+    expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+    userEvent.click(screen.getByText("Submit"));
+    expect(screen.getByText(/Valid!/)).toBeTruthy();
+  });
+
+  it("should error after entering and removing a character because required", async () => {
+    render(<KitchenSink />);
+    const domNode = screen.getByLabelText("Field") as HTMLInputElement;
+    userEvent.type(domNode, "1");
+    expect(domNode.value).toEqual("1");
+    userEvent.type(domNode, "{backspace}");
+    expect(domNode.value).toEqual("");
+
+    expect(screen.getByTestId(testIds.errorText)).toHaveTextContent(
+      "Yo, Field is required"
+    );
+    expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+    userEvent.click(screen.getByText("Submit"));
+    expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+  });
+
+  it("should show a custom error when entering '3'", async () => {
+    render(<KitchenSink />);
+    const domNode = screen.getByLabelText("Field") as HTMLInputElement;
+    userEvent.type(domNode, "3");
+    expect(domNode.value).toEqual("3");
+
+    expect(screen.getByTestId(testIds.errorText)).toHaveTextContent(
+      "3 is not allowed!"
+    );
+    expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+    userEvent.click(screen.getByText("Submit"));
+    expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+  });
+
+  describe("async", () => {
+    beforeEach(() => {
+      jest.useFakeTimers();
+    });
+    afterEach(() => {
+      jest.runOnlyPendingTimers();
+      jest.useRealTimers();
+    });
+
+    it("should show an async custom error when entering '4'", async () => {
+      render(<KitchenSink />);
+      const domNode = screen.getByLabelText("Field") as HTMLInputElement;
+      userEvent.type(domNode, "4");
+      expect(domNode.value).toEqual("4");
+
+      expect(screen.getByTestId(testIds.errorText)).toHaveTextContent("");
+      expect(screen.getByTestId(testIds.loading)).toHaveTextContent(
+        "...loading"
+      );
+
+      expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+      userEvent.click(screen.getByText("Submit"));
+      expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+
+      await act(async () => jest.runAllTimers());
+
+      expect(screen.getByTestId(testIds.errorText)).toHaveTextContent(
+        "4 is not allowed async!"
+      );
+      expect(screen.queryByTestId(testIds.loading)).not.toBeTruthy();
+
+      expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+      userEvent.click(screen.getByText("Submit"));
+      expect(screen.queryByText(/Valid!/)).not.toBeTruthy();
+    });
   });
 });
